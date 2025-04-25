5e063459252c45340f7c85db73c40037a9995ac1
Sergio Enrique Rojas Medina <69541817+seper7304@users.noreply.github.com>
2025-04-24 22:18:26 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,16 @@
-// config/db.js placeholder
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB conectado');
+  } catch (err) {
+    console.error('Error conectando a MongoDB:', err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
