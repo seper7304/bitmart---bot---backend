411f721b5d0c537079542d20bf3d65b67050c310
Sergio Enrique Rojas Medina <69541817+seper7304@users.noreply.github.com>
2025-04-24 22:22:25 -0600

    Update botRoutes.js

--- botRoutes.js
+++ botRoutes.js
@@ -1,1 +1,19 @@
 // routes/botRoutes.js placeholder
+const express = require('express');
+const router = express.Router();
+const {
+  createBot,
+  startBot,
+  stopBot,
+  getBotByUser,
+  updateBotStrategy
+} = require('../controllers/bot.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
+
+router.post('/', authMiddleware, createBot);
+router.get('/', authMiddleware, getBotByUser);
+router.post('/:botId/strategy/start', authMiddleware, startBot);
+router.post('/:botId/strategy/stop', authMiddleware, stopBot);
+router.put('/:botId/strategy', authMiddleware, updateBotStrategy);
+
+module.exports = router;
